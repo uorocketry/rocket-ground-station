60c1306e54a3bd2cb4c9906d17795bdc5c68d009
Fernando Nogueira <abert036@uottawa.ca>
2022-07-16 17:17:45 -0400

    feat: add json (de)serialize helpers

--- src/main/java/uorocketry/basestation/config/Config.java
+++ src/main/java/uorocketry/basestation/config/Config.java
@@ -1,6 +1,12 @@
 package uorocketry.basestation.config;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @lombok.Data
 public class Config {
@@ -13,4 +19,34 @@ public class Config {
     @lombok.Getter(onMethod_ = { @JsonProperty("stateEvents") })
     @lombok.Setter(onMethod_ = { @JsonProperty("stateEvents") })
     private StateEvent[] stateEvents;
+
+    // Serialize/deserialize helpers
+    public static Config fromJsonString(String json) throws IOException {
+        return getObjectReader().readValue(json);
+    }
+
+    public static String toJsonString(Config obj) throws JsonProcessingException {
+        return getObjectWriter().writeValueAsString(obj);
+    }
+
+    private static ObjectReader reader;
+    private static ObjectWriter writer;
+
+    private static void instantiateMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        reader = mapper.readerFor(Config.class);
+        writer = mapper.writerFor(Config.class);
+    }
+
+    private static ObjectReader getObjectReader() {
+        if (reader == null)
+            instantiateMapper();
+        return reader;
+    }
+
+    private static ObjectWriter getObjectWriter() {
+        if (writer == null)
+            instantiateMapper();
+        return writer;
+    }
 }
